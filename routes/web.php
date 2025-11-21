e04a3fec4b7410f8bea21b7b0d21a48ef5efdd1e
thienvolc <ttt2042005@gmail.com>
2025-11-21 23:21:50 +0700

    re-route

--- routes/web.php
+++ routes/web.php
@@ -6,3 +6,11 @@ require __DIR__ . '/web/sales.php';
 require __DIR__ . '/web/identity.php';
 require __DIR__ . '/web/content.php';
 require __DIR__ . '/web/shared.php';
+
+Route::prefix('admin')->group(function () {
+    require __DIR__ . '/admin/dashboard.php';
+    require __DIR__ . '/admin/catalog.php';
+    require __DIR__ . '/admin/sales.php';
+    require __DIR__ . '/admin/identity.php';
+    require __DIR__ . '/admin/content.php';
+});
